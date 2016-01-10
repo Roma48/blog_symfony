1a54c9f2847d683ca609118ce67609319c25a3a1
Roma48 <myzfann@mail.ru>
2016-01-10 21:49:40 +0200

    add internal page

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -7,18 +7,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class ArticleController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/article", name="article")
      * @Template()
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('article/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
             'class' => $request->attributes->get('_route')
-        ));
+            )
+        );
     }
 }
